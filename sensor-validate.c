775960aaffbbc0947e4c7c6b0c6f5bd3b93bff4d
Gagandeep-Gawadia <77660067+Gagandeep-Gawadia@users.noreply.github.com>
2021-03-26 00:36:36 +0530

    Resolving duplication

--- sensor-validate.c
+++ sensor-validate.c
@@ -1,28 +1,44 @@
 #include "sensor-validate.h"
 
-int _give_me_a_good_name(double value, double nextValue, double maxDelta) {
+#define soc_delta 0.05f
+#define currentreading_delta 0.1f
+
+int suddenJumpInDataIsDetected(double value, double nextValue, double maxDelta) {
   if(nextValue - value > maxDelta) {
     return 0;
   }
   return 1;
 }
 
-int validateSOCreadings(double* values, int numOfValues) {
-  int lastButOneIndex = numOfValues - 1;
-  for(int i = 0; i < lastButOneIndex; i++) {
-    if(!_give_me_a_good_name(values[i], values[i + 1], 0.05)) {
-      return 0;
-    }
-  }
-  return 1;
+int parametersAreValidIfSet(double* values, int numOfValues, float deltadifference) {
+	int lastButOneIndex = numOfValues - 1;
+	for (int i = 0; i < lastButOneIndex; i++) {
+		if (!suddenJumpInDataIsDetected(values[i], values[i + 1], deltadifference)) {
+			return 0;
+		}
+	}
+	return 1;
 }
 
-int validateCurrentreadings(double* values, int numOfValues) {
-  int lastButOneIndex = numOfValues - 1;
-  for(int i = 0; i < lastButOneIndex; i++) {
-    if(!_give_me_a_good_name(values[i], values[i + 1], 0.1)) {
-      return 0;
-    }
-  }
-  return 1;
+int SensorValidation(struct validationdataset * Sensordataset,enum typeofcommunicationerror communicationfailuredetails)
+{   
+	if (Sensordataset != NULL)
+	{   
+		communicationfailuredetails = nocommunicationfailure;
+
+		int currentParIsValidatedIfTrue = 1;
+		int sOCParIsValidatedIfTrue = 1;
+		int sensorIsHealthyIfTrue = 1;
+
+		sOCParIsValidatedIfTrue = parametersAreValidIfSet(Sensordataset->values_soc, Sensordataset->numOfdataset_soc, soc_delta);
+		currentParIsValidatedIfTrue = parametersAreValidIfSet(Sensordataset->values_current, Sensordataset->numOfdataset_current, currentreading_delta);
+
+		sensorIsHealthyIfTrue = (sOCParIsValidatedIfTrue&currentParIsValidatedIfTrue);
+		return  sensorIsHealthyIfTrue;
+	}
+	else
+	{
+		communicationfailuredetails = communicationfailureothers;
+		return 0;
+	}
 }
