2e7e2463810d10bf07a95ebfb3ec8a14264aaa48
Gagandeep-Gawadia <77660067+Gagandeep-Gawadia@users.noreply.github.com>
2021-03-26 01:33:49 +0530

    adapting file for typedef header inclusion

--- sensor-validate.h
+++ sensor-validate.h
@@ -1,23 +1,5 @@
 //#include <stdbool.h>
-#define maxnumofdataset 10
-
-enum typeofcommunicationerror
-{
-	nocommunicationfailure,
-	communicationfailedwithsocsensor,
-	communicationfailedwithcurrentsensor,
-	communicationfailureothers
-};
-
-struct validationdataset
-{
-	double  values_soc[maxnumofdataset];
-	int     numOfdataset_soc;
-	bool    status_socreading;
-	double  values_current[maxnumofdataset];
-	int     numOfdataset_current;
-	bool    status_currentreading;
-};
+#include "typedef.h"
 
 int parametersAreValidIfSet(double* values, int numOfValues, float deltadifference);
 int SensorValidation(struct validationdataset * Sensordataset, enum typeofcommunicationerror communicationfailuredetails);
