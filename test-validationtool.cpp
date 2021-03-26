86df4a00db0c2ea970f8da9f61bf027d684459be
Gagandeep-Gawadia <77660067+Gagandeep-Gawadia@users.noreply.github.com>
2021-03-26 19:15:19 +0530

    correcting failing tests

--- test-validationtool.cpp
+++ test-validationtool.cpp
@@ -11,7 +11,7 @@ TEST_CASE("reports error when soc jumps abruptly") {
 	 double socReadings[] =     { 0.0, 0.01, 0.5, 0.51 };
 	 double currentReadings[] = { 0.0, 0.02, 0.03, 0.02 };
 
-  	REQUIRE((!SensorValidation( socReadings, 4, currentReadings,4, communicationfailuredetails)) == 0);
+  	REQUIRE((SensorValidation( socReadings, 4, currentReadings,4, communicationfailuredetails)) == 0);
 	
 }
 TEST_CASE("reports error when current jumps abruptly") {
@@ -22,7 +22,7 @@ TEST_CASE("reports error when current jumps abruptly") {
 	 double socReadings_1[] =  { 0.01, 0.01, 0.04, 0.02 };
 	 double currentReadings_1[] = { 0.0, 0.02, 0.03, 0.33 };
          
-	REQUIRE((!SensorValidation( socReadings_1 ,4, currentReadings_1, 4, communicationfailuredetails_1)) == 0);
+	REQUIRE((SensorValidation( socReadings_1 ,4, currentReadings_1, 4, communicationfailuredetails_1)) == 0);
 	
 }
 TEST_CASE("reports error when empty array is passed ") {
@@ -33,7 +33,7 @@ TEST_CASE("reports error when empty array is passed ") {
 	 double socReadings_2[] =     { 0.0, 0.01, 0.5, 0.51 };
 	 double currentReadings_2[] = { 0.0, 0.02, 0.03, 0.02 };
 
-  	REQUIRE((!SensorValidation( socReadings_2 , 0, currentReadings_2 ,0, communicationfailuredetails_2)) == 0);
+  	REQUIRE((SensorValidation( socReadings_2 , 0, currentReadings_2 ,0, communicationfailuredetails_2)) == 0);
 	
 }
 
