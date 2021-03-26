f1b10b4703181e9bec50b6c58573b699fd55e839
Gagandeep-Gawadia <77660067+Gagandeep-Gawadia@users.noreply.github.com>
2021-03-26 18:57:44 +0530

    Adding empty array case

--- test-validationtool.cpp
+++ test-validationtool.cpp
@@ -11,7 +11,7 @@ TEST_CASE("reports error when soc jumps abruptly") {
 	 double socReadings[] =     { 0.0, 0.01, 0.5, 0.51 };
 	 double currentReadings[] = { 0.0, 0.02, 0.03, 0.02 };
 
-  	REQUIRE((!SensorValidation( socReadings,4, currentReading,4, communicationfailuredetails)) == 0);
+  	REQUIRE((!SensorValidation( socReadings, 4, currentReadings,4, communicationfailuredetails)) == 0);
 	
 }
 TEST_CASE("reports error when current jumps abruptly") {
@@ -22,7 +22,18 @@ TEST_CASE("reports error when current jumps abruptly") {
 	 double socReadings[] =  { 0.01, 0.01, 0.04, 0.02 };
 	 double currentReadings[] = { 0.0, 0.02, 0.03, 0.33 };
          
-	REQUIRE((!SensorValidation( socReadings,4, currentReading,4, communicationfailuredetails)) == 0);
+	REQUIRE((!SensorValidation( socReadings,4, currentReadings, 4, communicationfailuredetails)) == 0);
+	
+}
+TEST_CASE("reports error when empty array is passed ") {
+        
+
+	 typeofcommunicationerror communicationfailuredetails[2] = { nocommunicationfailure,nocommunicationfailure };
+
+	 double socReadings[] =     { 0.0, 0.01, 0.5, 0.51 };
+	 double currentReadings[] = { 0.0, 0.02, 0.03, 0.02 };
+
+  	REQUIRE((!SensorValidation( socReadings, 0, currentReadings,0, communicationfailuredetails)) == 0);
 	
 }
 
