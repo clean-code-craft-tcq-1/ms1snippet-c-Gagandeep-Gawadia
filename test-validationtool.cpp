1bdc4ad95bda8f9f4ee1b7451ce558d49f202733
Gagandeep-Gawadia <77660067+Gagandeep-Gawadia@users.noreply.github.com>
2021-03-26 18:55:22 +0530

    Update test-validationtool.cpp

--- test-validationtool.cpp
+++ test-validationtool.cpp
@@ -11,9 +11,7 @@ TEST_CASE("reports error when soc jumps abruptly") {
 	 double socReadings[] =     { 0.0, 0.01, 0.5, 0.51 };
 	 double currentReadings[] = { 0.0, 0.02, 0.03, 0.02 };
 
-  	REQUIRE((!SensorValidation( socReadings,
-		( sizeof(socReadings) / sizeof(socReadings[0])), currentReadings ,
-		 (sizeof(currentReadings) / sizeof(currentReadings[0])), communicationfailuredetails)) == 0);
+  	REQUIRE((!SensorValidation( socReadings,4, currentReading,4, communicationfailuredetails)) == 0);
 	
 }
 TEST_CASE("reports error when current jumps abruptly") {
@@ -24,9 +22,7 @@ TEST_CASE("reports error when current jumps abruptly") {
 	 double socReadings[] =  { 0.01, 0.01, 0.04, 0.02 };
 	 double currentReadings[] = { 0.0, 0.02, 0.03, 0.33 };
          
-	 REQUIRE((!SensorValidation( socReadings,
-		( sizeof(socReadings) / sizeof(socReadings[0])), currentReadings ,
-		 (sizeof(currentReadings) / sizeof(currentReadings[0])), communicationfailuredetails)) == 0);
+	REQUIRE((!SensorValidation( socReadings,4, currentReading,4, communicationfailuredetails)) == 0);
 	
 }
 
