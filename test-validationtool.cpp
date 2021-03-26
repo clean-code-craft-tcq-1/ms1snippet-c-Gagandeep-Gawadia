b446aa4b223fee44cd21d3eb96c1934bbaafc7d5
Gagandeep-Gawadia <77660067+Gagandeep-Gawadia@users.noreply.github.com>
2021-03-26 21:04:28 +0530

    removing warning

--- test-validationtool.cpp
+++ test-validationtool.cpp
@@ -4,7 +4,7 @@
  ***
  ***    \brief       file contains test cases which belongs to file 
                      sensor-validate.c 
-/*****************************************************************************/
+*******************************************************************************/
 
 /*-------------------------------  Include files  -------------------------------------------*/
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
