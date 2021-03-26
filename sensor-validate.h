5da20b0d59acfda4b47218b365f10e0b93ef5390
Gagandeep-Gawadia <77660067+Gagandeep-Gawadia@users.noreply.github.com>
2021-03-26 17:02:32 +0530

    Added more functionality

--- sensor-validate.h
+++ sensor-validate.h
@@ -1,6 +1,10 @@
-//#include <stdbool.h>
+
 #include "typedef.h"
 
-int parametersAreValidIfSet(double* values, int numOfValues, float deltadifference);
-int SensorValidation(struct validationdataset * Sensordataset, enum typeofcommunicationerror communicationfailuredetails);
-int suddenJumpInDataIsDetected(double value, double nextValue, double maxDelta);
+int parametersAreValidIfSet(double* values, int numOfValues, float deltadifference, parametername individualpar, typeofcommunicationerror *communicationfailuredetails);
+int SensorValidation( validationdataset * Sensordataset, typeofcommunicationerror *communicationfailuredetails);
+int NospikeIsDetected(double value, double nextValue, double maxDelta);
+void runSensorAnalysis();
+void readData( validationdataset *Sensordataset);
+datamessage evaluateBreachState(typeofcommunicationerror *communicationfailuredetails);
+void printToConsole(datamessage breach, int sensornoisyisfalse);
