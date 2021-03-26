d98e323bedcbf6a6d7e77dafd6f34abe6eae0bf1
Gagandeep-Gawadia <77660067+Gagandeep-Gawadia@users.noreply.github.com>
2021-03-26 18:47:50 +0530

    Resolving test duplication

--- sensor-validate.h
+++ sensor-validate.h
@@ -2,10 +2,8 @@
 #include "typedef.h"
 
 int parametersAreValidIfSet(double* values, int numOfValues, float deltadifference, parametername individualpar, typeofcommunicationerror *communicationfailuredetails);
-int SensorValidation( validationdataset * Sensordataset, typeofcommunicationerror *communicationfailuredetails);
+int SensorValidation(double * values_soc, int numOfdataset_soc, double * values_current, int numOfdataset_current, typeofcommunicationerror *communicationfailuredetails);
 int NospikeIsDetected(double value, double nextValue, double maxDelta);
-void runSensorAnalysis();
-void readData( validationdataset *Sensordataset);
 datamessage evaluateBreachState(typeofcommunicationerror *communicationfailuredetails);
 void printToConsole(datamessage breach, const char * sensorstate);
 int evaluationofspikedetected(double* values, int numOfValues, float deltadifference);
