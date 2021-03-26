d0b46bd8e2b336ab615dc0eb97d9fa834fc40424
Gagandeep-Gawadia <77660067+Gagandeep-Gawadia@users.noreply.github.com>
2021-03-26 20:54:07 +0530

    file header included

--- sensor-validate.h
+++ sensor-validate.h
@@ -1,6 +1,14 @@
+/******************************************************************************/
+/***    \file        sensor-validate.h
+ ***    \author      Gagandeep Gawadia
+ ***
+ ***    \brief       File contains declaration for sensor-validate.c functions 
+/*****************************************************************************/
 
+/*----------------------------------Include file ------------------------------*/
 #include "typedef.h"
-
+ 
+/*---------------------------------- Function declaration ------------------------------*/
 int parametersAreValidIfSet(double* values, int numOfValues, float deltadifference, parametername individualpar, typeofcommunicationerror *communicationfailuredetails);
 int SensorValidation(double * values_soc, int numOfdataset_soc, double * values_current, int numOfdataset_current, typeofcommunicationerror *communicationfailuredetails);
 int NospikeIsDetected(double value, double nextValue, double maxDelta);
